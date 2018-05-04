bd2a01ee23f894189c74f91d0d7ace466327e73a
Gary Hall <guategary@gmail.com>
2018-05-03 23:08:07 -0400

    Add eslint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-"use strict";
-
+// eslint-disable-next-line import/no-extraneous-dependencies
 require('normalize.css');
+
+// normalize.css must be loaded first before app css, so disable eslint check
+/* eslint-disable import/first */
 import './assets/css/style.scss';
+/* eslint-enable import/first */
